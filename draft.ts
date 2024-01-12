cd004cd544ce1106e0c77979252ca572aa74e9c9
Denis Obolevich <denzelwash33@yandex.ru>
2024-01-12 22:30:52 +0300

    added an example of union types to the draft

--- draft.ts
+++ draft.ts
@@ -1,4 +1,4 @@
-// Base type
+// Base types
 const a: string = 'string'
 const b: number = 3
 const c: number = 5
@@ -73,3 +73,32 @@ if (res.statusCode === 1) {
 function action(status: StatusCode) {}
 
 action(StatusCode.SUCCESS)
+
+// Union types
+const unionArr: (string | number)[] = ['string', 5]
+
+function logId(id: string | number | boolean) {
+	if (typeof id === 'string') {
+		console.log(id.toUpperCase())
+	} else if (typeof id === 'number') {
+		console.log(id.toString())
+	} else {
+		console.log(id)
+	}
+}
+
+function logError(err: string | string[]) {
+	if (Array.isArray(err)) {
+		console.log(err)
+	} else {
+		console.log(err)
+	}
+}
+
+function logObj(obj: { a: 1 } | { b: 1 }) {
+	if ('a' in obj) {
+		console.log(obj.a)
+	} else {
+		console.log(obj.b)
+	}
+}
