8f7c19c438ebb95307ee13fa3eca49e26e34f60f
Denis Obolevich <denzelwash33@yandex.ru>
2024-01-14 15:42:48 +0300

    added an example of never

--- examples.ts
+++ examples.ts
@@ -247,3 +247,31 @@ async function fethDataUnknown() {
 		}
 	}
 }
+
+// Never
+function generateError(message: string): never {
+	throw new Error(message)
+}
+
+function dumpError(): never {
+	while (true) {}
+}
+
+type paymentAction = 'refund' | 'checkout' | 'reject'
+
+function processAction(action: paymentAction) {
+	switch (action) {
+		case 'refund':
+			// ...
+			break
+		case 'checkout':
+			// ...
+			break
+		case 'reject':
+			// ...
+			break
+		default:
+			const _: never = action
+			throw new Error('Нет такого action')
+	}
+}
