75287c60cb5c49c60482d8a14c3c34a632bfe0cf
Denis Obolevich <denzelwash33@yandex.ru>
2024-01-14 15:56:41 +0300

    added an example of null

--- examples.ts
+++ examples.ts
@@ -275,3 +275,26 @@ function processAction(action: paymentAction) {
 			throw new Error('Нет такого action')
 	}
 }
+
+// Null
+const n1: null = null
+const n2: any = null
+
+interface IUser {
+	name: string
+}
+
+function getUserNull() {
+	if (Math.random() > 0.5) {
+		return null
+	} else {
+		return {
+			name: 'Name'
+		} as IUser
+	}
+}
+
+const userNull = getUserNull()
+if (userNull) {
+	console.log(userNull.name)
+}
