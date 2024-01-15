8142e2ee3a0154cda82ec6e92bfabbad286aef76
Denis Obolevich <denzelwash33@yandex.ru>
2024-01-15 21:51:48 +0300

    added an example of oop methods

---
+++ oop/3_methods.ts
@@ -0,0 +1,31 @@
+enum PaymentStatus {
+	Holded,
+	Processed,
+	Reversed
+}
+
+class Payment {
+	id: number
+	status: PaymentStatus = PaymentStatus.Holded
+	createdAt: Date = new Date()
+	updatedAt: Date
+	constructor(id: number) {
+		this.id = id
+	}
+
+	getPaymentLifeTime(): number {
+		return new Date().getTime() - this.createdAt.getTime()
+	}
+
+	unholdPayment(): void {
+		if (this.status === PaymentStatus.Processed) {
+			throw new Error('Платеж не может быть возвращен!')
+		}
+		this.status = PaymentStatus.Reversed
+		this.updatedAt = new Date()
+	}
+}
+
+const payment = new Payment(1)
+payment.unholdPayment()
+const time = payment.getPaymentLifeTime()
