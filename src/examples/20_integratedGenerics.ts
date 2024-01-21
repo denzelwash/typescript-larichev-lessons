69f19d14a829fd711b546cc7ca6be8d15e35e9ce
Denis Obolevich <denzelwash33@yandex.ru>
2024-01-21 11:50:46 +0300

    added an example of integrated generics

---
+++ src/examples/20_integratedGenerics.ts
@@ -0,0 +1,10 @@
+const num: Array<number> = [1, 2, 3]
+
+async function test() {
+  const a = await new Promise<number>((res, rej) => res(1))
+}
+
+const check: Record<string, boolean> = {
+  drive: true,
+  kpp: false
+}
