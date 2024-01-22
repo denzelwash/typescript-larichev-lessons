282d180ecacead67cd721dd28f762e3e9c80bb78
Denis Obolevich <denzelwash33@yandex.ru>
2024-01-22 21:42:03 +0300

    added an example of extends generics

---
+++ src/examples/23_extendsGenerics.ts
@@ -0,0 +1,20 @@
+class Vehicle {
+  run: number
+}
+
+function kmToMiles<T extends Vehicle>(vehicle: T): T {
+  vehicle.run = vehicle.run / 0.62
+  return vehicle
+}
+
+class LCV extends Vehicle {
+  capacity: number
+}
+
+const vehicle = kmToMiles(new Vehicle())
+const lcv = kmToMiles(new LCV())
+
+function logId<T extends string | number, Y>(id: T, additionalData: Y): { id: T; data: Y } {
+  console.log(id, additionalData)
+  return { id, data: additionalData }
+}
