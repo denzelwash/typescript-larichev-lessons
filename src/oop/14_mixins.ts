8f0f5483aa95b5a14c13ea70ff47e8ea20d35a0f
Denis Obolevich <denzelwash33@yandex.ru>
2024-01-23 19:43:25 +0300

    added an example of mixins

---
+++ src/oop/14_mixins.ts
@@ -0,0 +1,36 @@
+type Constructor = new (...args: any[]) => {}
+type GConstructor<T = {}> = new (...args: any[]) => T
+
+class List {
+  constructor(public items: string[]) {}
+}
+
+class Accordion {
+  isOpened: boolean
+}
+
+type ListType = GConstructor<List>
+type AccordionType = GConstructor<Accordion>
+
+class ExtendedListClass extends List {
+  first() {
+    return this.items[0]
+  }
+}
+
+function ExtendedList<TBase extends ListType & AccordionType>(Base: TBase) {
+  return class ExtendedList extends Base {
+    first() {
+      return this.items[0]
+    }
+  }
+}
+
+class AccordionList {
+  isOpened: boolean
+  constructor(public items: string[]) {}
+}
+
+const list = ExtendedList(AccordionList)
+const res = new list(['a', 'b'])
+console.log(res.first())
