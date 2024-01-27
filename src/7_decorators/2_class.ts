02f4ed7c43ebd5976027b5f49f9cb6c649939451
Denis Obolevich <denzelwash33@yandex.ru>
2024-01-27 11:13:16 +0300

    adden an example of decorator class

---
+++ src/7_decorators/2_class.ts
@@ -0,0 +1,25 @@
+interface IUSerService {
+  users: number
+  getUsersInDatabase(): number
+}
+
+@nullUser
+@threeUserAdvanced
+class UserService implements IUSerService {
+  users: number = 1000
+  getUsersInDatabase(): number {
+    return this.users
+  }
+}
+
+function nullUser(target: Function) {
+  target.prototype.users = 0
+}
+
+function threeUserAdvanced<T extends { new (...args: any[]): {} }>(constructor: T) {
+  return class extends constructor {
+    users = 3
+  }
+}
+
+console.log(new UserService().getUsersInDatabase())
