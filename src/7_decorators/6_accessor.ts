abf113f73aac51b2b39dd77fd872a9bafd56e028
Denis Obolevich <denzelwash33@yandex.ru>
2024-01-29 23:22:27 +0300

    adden an example of decorator accessor

---
+++ src/7_decorators/6_accessor.ts
@@ -0,0 +1,35 @@
+interface IUSerService {
+  users: number
+  getUsersInDatabase(): number
+}
+
+class UserService implements IUSerService {
+  private _users: number
+
+  @Log()
+  set users(num: number) {
+    this._users = num
+  }
+
+  get users() {
+    return this._users
+  }
+
+  getUsersInDatabase(): number {
+    throw new Error('Ошибка')
+  }
+}
+
+function Log() {
+  return (target: Object, propertyKey: string | symbol, descriptor: PropertyDescriptor) => {
+    const set = descriptor.set
+    descriptor.set = (...args: any) => {
+      console.log(args)
+      set?.apply(target, args)
+    }
+  }
+}
+
+const userService = new UserService()
+userService.users = 1
+console.log(userService.users)
